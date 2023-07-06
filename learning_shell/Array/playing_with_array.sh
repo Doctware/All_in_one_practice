0122cc8e68edc1f448ae2e621649d8ae5b2ac56a
Doctware <yusclever9@gmail.com>
2023-07-06 14:18:30 +0100

    still working with array

--- learning_shell/Array/playing_with_array.sh
+++ learning_shell/Array/playing_with_array.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 my_array=(apple banana "Doctare" orange)
 new_array[2]=apricot
+my_array[6]="Eba"
 
 echo ${#my_array[@]}
+echo ${my_array[0]}
+echo ${my_array[1]}
+echo ${my_array[2]}
+echo ${my_array[3]}
+echo ${my_array[6]}
+echo ${my_array[${#my_array[@]}-2]}

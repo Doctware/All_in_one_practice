e7fcd81bae8977133dfb354d0311aba994609341
Doctware <yusclever9@gmail.com>
2023-11-14 23:02:37 +0100

    the header file

--- playing_withC/Automatic_Dynamic_alocationMalloc_and_free/doctware.h
+++ playing_withC/Automatic_Dynamic_alocationMalloc_and_free/doctware.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c, char k);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* doctware.h */

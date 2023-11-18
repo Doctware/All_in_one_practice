6030aeb141ebf4b6ef08671cf401f498eb39bb8c
Doctware <yusclever9@gmail.com>
2023-11-18 16:59:23 +0100

    Header file

---
+++ playing_withC/Automatic_Dynamic_alocationMalloc_and_free/More_Malloc_free/doctware.h
@@ -0,0 +1,12 @@
+#ifndef DOCTWARE_H
+#define DOCTWARE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned  int n);
+
+#endif /* doctware.h */

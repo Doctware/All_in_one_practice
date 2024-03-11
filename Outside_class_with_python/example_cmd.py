ef332c6b27b436811522552fc7ec2a88773e5e24
Doctware <yusclever9@gmail.com>
2024-03-11 09:40:25 +0100

    :)

--- Outside_class_with_python/example_cmd.py
+++ Outside_class_with_python/example_cmd.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
 
 import cmd
+import shlex
 
 class HelloWorld(cmd.Cmd):
     """ simple command processor example """
+    intro = "Doctware App"
+    prompt = "please enter a command: "
 
     freinds = ['Alice', 'Doctware', 'Yusuf', 'Bob']
 
     def do_great(self, person):
         if person and person in self.freinds:
-            greeting = "hello {:s}".formart(person)
+            greeting = "hello {:s}".format(person)
         elif person:
             greeting = "hello, " + person
         else:
             greeting = 'hello'
-        print(greeting)
+        print(shlex.split(greeting))
 
     def complete_great(self, text, line, beidx, endidx):
         if not text:
             completion = self.freinds[:]
         else:
-            completion = freinds[f 
-                                 for f in self.freinds
-                                 if f.startswith(text)
-                                 ]
+            completion = [f for f in self.freinds if f.startswith(text) ]
         return completion
 
     def help_great(self):
         print('\n'.join(['greet [name_of_person]','Greet the named person']))
+
+    def do_sum(self, nums):
+        total = sum(map(int, shlex.split(nums)))
+        print("{:d}".format(total))
 
     def do_EOF(self, line):
         return True

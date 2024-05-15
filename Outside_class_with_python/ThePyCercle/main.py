5ddc623417c7152e7efd856bf82bc4f8acb513c0
Doctware <yusclever9@gmail.com>
2024-05-15 23:40:17 +0100

    refreshing

--- Outside_class_with_python/ThePyCercle/main.py
+++ Outside_class_with_python/ThePyCercle/main.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 from model.base import Base
+from model.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    b1 = Base(10)
+    print("Base model section")
+    print()
+
+    b1 = Base()
     print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base(99456)
+    print(b3.id)
+
+    print("---------")
+    print()
+
+    print("Rectangle section")
+    print()
+
+    r1 = Rectangle()
+    print(r1.id)
+
+    r2 = Rectangle()
+    print(r2.id)
--- Outside_class_with_python/ThePyCercle/model/rectangle.py
+++ Outside_class_with_python/ThePyCercle/model/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ this module contains the class rectangle """
-
+from model.base import Base
 
 
 class Rectangle(Base):
-    """ The class rectangle thats inherit from Bse class """
+    """ The class rectangle thats inherit from Base class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
@@ -32,6 +32,7 @@ class Rectangle(Base):
         """
         set new value to the width
         """
+        self.__width = value
 
     @property
     def height(self):
@@ -41,7 +42,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ assignd new value to Height """
-        return self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -51,7 +52,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, balue):
         """ assign value to x """
-        return self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -61,5 +62,5 @@ class Rectangle(Base):
     @x.setter
     def y(self, value):
         """ assign value to y """
-        return self.__y = value
+        self.__y = value
 

5d96452c260e053cfd0fc01f74592fbbce0a5e5c
Doctware <yusclever9@gmail.com>
2024-05-18 15:58:52 +0100

    Practicing

--- Outside_class_with_python/example_cmd.py
+++ Outside_class_with_python/example_cmd.py
@@ -1,40 +1,26 @@
 #!/usr/bin/env python3
-
 import cmd
-import shlex
 
 class HelloWorld(cmd.Cmd):
     """ simple command processor example """
-    intro = "Doctware App"
-    prompt = "please enter a command: "
 
-    freinds = ['Alice', 'Doctware', 'Yusuf', 'Bob']
-
-    def do_great(self, person):
-        if person and person in self.freinds:
-            greeting = "hello {:s}".format(person)
-        elif person:
-            greeting = "hello, " + person
+    def do_greet(self, person):
+        """ greet [person]
+        gree the name of person """
+        if person:
+            print("hello", person)
         else:
-            greeting = 'hello'
-        print(shlex.split(greeting))
+            print("hello")
 
-    def complete_great(self, text, line, beidx, endidx):
-        if not text:
-            completion = self.freinds[:]
-        else:
-            completion = [f for f in self.freinds if f.startswith(text) ]
-        return completion
-
-    def help_great(self):
-        print('\n'.join(['greet [name_of_person]','Greet the named person']))
-
-    def do_sum(self, nums):
-        total = sum(map(int, shlex.split(nums)))
-        print("{:d}".format(total))
+    def help_greet(self):
+        print('\n'.join(['greet [person,]',
+                            'greet the name of person']))
 
     def do_EOF(self, line):
         return True
+
+    def do_postloop(self):
+        print()
 
 if __name__ == "__main__":
     HelloWorld().cmdloop()
